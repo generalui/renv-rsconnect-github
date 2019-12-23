477e7b9de0c4ddd62529e17cae872f18e63c9f65
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2019-12-23 10:49:54 -0800

    INITIAL

---
+++ app.R
@@ -0,0 +1,16 @@
+print("App.R - concordanceIndex dependency...")
+concordanceIndex::concordanceIndex
+
+# Simple Shiny App based on:
+# https://github.com/rstudio/shiny-examples/tree/master/084-single-file
+shinyApp(
+  ui = bootstrapPage(
+    numericInput('n', 'Number of objects', 200,
+    plotOutput('plot')
+  ),
+  server = function(input, output) {
+    output$plot <- renderPlot({
+      hist(runif(input$n))
+    })
+  }
+)
